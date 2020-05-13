4c113834a99c40aa6c960050f4a2f1257d7bd217
eweck <edgargandolfi@gmail.com>
2020-05-13 15:46:35 +0200

    sl-edgar

---
+++ 02-PB-operators-arithmetic-pt1/ex-02.js
@@ -0,0 +1,23 @@
+//Declaring variables
+let x = 3;
+let y = 4;
+let z = 10;
+let a = 15;
+let b = 9;
+let c = 20;
+let d = b - a;
+let resultOne = x * y;
+let resultTwo = (a + b) * c; 
+
+//printing results
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log((x * y) / z);
+console.log(resultOne);
+console.log(resultTwo);
+console.log(++a);
+console.log(--b);
+console.log(d + c);
+console.log(resultOne % resultTwo)
