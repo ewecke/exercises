46044580b80abcc18b22c8d8825075476fd04cd6
eweck <edgargandolfi@gmail.com>
2020-05-14 11:46:29 +0200

    sl-edgar

---
+++ 03-PB-console/ex-03.js
@@ -0,0 +1,22 @@
+//declarating variables
+//task 1 to 3
+let myVariable = "This is the value.";
+let firstName = "Edgar";
+let lastName = "Weckelmann Gandolfi";
+//task 4
+let profileName = "Kurt Donald Cobain";
+let profileNationality = "American";
+let profileJob = "Rock Star";
+let profileDob = "20-02-1967";
+let profileDod = "5-04-1994";
+
+
+//print results
+//task 1 to 3
+console.log(myVariable);
+console.log(firstName, lastName)
+//task 4
+ console.log(profileName, profileNationality, profileJob, profileDob, profileDod) 
+ //task 5
+
+ console.log(profileName +=" was a", profileNationality, profileJob +=" Born on", profileDob +=" and die on", profileDod) 
