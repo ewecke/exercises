b59f485d441f312b2b12e2ca20e1974598bd7e0d
eweck <edgargandolfi@gmail.com>
2020-05-14 11:54:10 +0200

    git-fix-edgar

--- 03-PB-console/ex-03.js
+++ 03-PB-console/ex-03.js
@@ -10,13 +10,11 @@ let profileJob = "Rock Star";
 let profileDob = "20-02-1967";
 let profileDod = "5-04-1994";
 
-
 //print results
 //task 1 to 3
 console.log(myVariable);
 console.log(firstName, lastName)
 //task 4
- console.log(profileName, profileNationality, profileJob, profileDob, profileDod) 
- //task 5
-
- console.log(profileName +=" was a", profileNationality, profileJob +=" Born on", profileDob +=" and die on", profileDod) 
+console.log(profileName, profileNationality, profileJob, profileDob, profileDod) 
+//task 5
+console.log(profileName +=" was a", profileNationality, profileJob +=" Born on", profileDob +=" and die on", profileDod) 
