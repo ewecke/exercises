a07b060d02511fe503551a88a7d989a574a08a80
eweck <edgargandolfi@gmail.com>
2020-05-18 18:47:51 +0200

    ex-06-edgar

---
+++ 06-PB-ternary-exercises/ex-06-edgar.js
@@ -0,0 +1,21 @@
+"use strict";
+//Q1
+let isDog = false;
+console.log(isDog === true ? 'pat, pat' : 'find me a dog to pat!');
+
+//Q2
+let speedCheck = 200;
+let speedLimit = 50;
+console.log(speedCheck >= speedLimit ? 'you\'re going too fast!' : 'You\'re driving below the speed limit, Oma');
+
+//Q3
+let age = 40;
+console.log(age < 16 ? "serve butter beer" : "serve beer");
+
+//Q4
+let isStudent = true;
+console.log(isStudent = true ? "Ticket costs €5,00" : "Ticket costs €12,00");
+
+//Q5
+let okMarie = true;
+console.log(okMarie = true ? "Let them eat cake" : "Oh, bother");
