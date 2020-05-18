90def4441138a671a47524eafd427f3061354d59
eweck <edgargandolfi@gmail.com>
2020-05-18 19:53:47 +0200

    ex-07-sl-edgar

---
+++ 07-PB-data-types-pt1/ex-07-edgar.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//Q1
+let noName = "paralelepipedo";
+console.log(noName);
+
+//Q2
+console.log(noName[1] + noName[5]);
+
+//Q3
+let name = `Edgar`;
+
+//Q4
+let age = 40;
+
+//Q5
+let year = 2020;
+
+//Q6
+console.log(`${name} is ${age} in ${year}`);
+
+//Q7
+let isMarried = false;
+
+//Q8
+console.log(`${name} is married: ${isMarried}`);
+
+//Q9
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(`${programmingLanguage} is Fun: ${isFun}`);
+
+//Q10
+console.log(`${programmingLanguage[0]+programmingLanguage[4]}`);
+
+//Q11
+console.log(`The length of the string is = ${programmingLanguage.length}`);
