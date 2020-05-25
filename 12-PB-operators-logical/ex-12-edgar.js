f0746363526e402c009d9695e4fc78faf7fc4db6
eweck <edgargandolfi@gmail.com>
2020-05-26 01:48:44 +0200

    ex-13-sl-edga

--- 12-PB-operators-logical/ex-12-edgar.js
+++ 12-PB-operators-logical/ex-12-edgar.js
@@ -11,15 +11,15 @@ console.log(!a && !b ? `a not b = true` : `a not b = false`); // i'm not sure
 let x = 11;
 let y = 13;
 let z = 17;
-    //Q2a
+    //Q3a
     console.log((x > z) && (x > y) ? `x is greater than z AND x is greater than y ? yes it is` : `x is greater than z AND x is greater than y ? no it is not`);
-    //Q2b
+    //Q3b
     console.log((x !== y) ? `x is not equal to y` : `x is equal to y`);
-    //Q2c
+    //Q3c
     console.log((z < y ) || ( z > x) ? `z is less than y OR z is greater than x` : `z is not less than y OR z is not greater than x`);
-    //Q2d
+    //Q3d
     console.log((x === z) || (x !== y) ? `x is equal to z OR x is NOT equal to y.` :`x is not equal to z OR x is equal to y.`);
-    //Q2e
+    //Q3e
     console.log(((x >= 10) && (y <= 10)) ? `x is greater than or equal to 10 AND y is less than or equal to 10` : `x is not greater than or equal to 10 AND y is not less than or equal to 10`);
     //Q3f
     console.log(((x * z) < 100) || ((x * y) > 100) ? `x multiplied by z is less than 100 OR whether x multiplied by y is greater than 100` : `x multiplied by z is not less than 100 OR nether x multiplied by y is greater than 100` );
---
+++ 13-PB-language-boolean/ex-13-edgar.js
@@ -0,0 +1,15 @@
+`use strict`;
+//Q1
+console.log(3 == "3" ? true : false );//compare values of the variable and not the type.
+console.log(3 === "3" ? true : false );//compare values and type.
+// What would happen if we were to use `=`? we assign a value to the variable.
+//Q2
+let a = true;
+console.log(!a ? true : false);
+
+//Q3
+let firstName, givenName; //opening empty variables
+
+firstName = 'Stacey'; // assign a value of `Stacey` to the varieble (firstName) 
+let name = givenName || firstName || 'John';// assign a value of (givenName or firstName or 'John'), becouse (firstName) is the first with value = true, js stop looking to the other elements and it is the new value of (name).
+console.log(name); // it is only printing on the console the log of the varible name.
