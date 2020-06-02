cfef70d92e03958e52107d16c038041c80e6b684
eweck <edgargandolfi@gmail.com>
2020-06-02 15:34:11 +0200

    ex-21-edgar

---
+++ 21-PB-program-repetition2/ex-21-edgar.js
@@ -0,0 +1,22 @@
+`use strict`;
+//Q1
+let i = 0;
+while (i < 15) { 
+    console.log(`${i}`);
+    i++;
+}
+
+//Q2
+let i2 = 0;
+while (i2 < 20) { 
+    i2++;
+    console.log(`${i2}`);
+}
+
+//Q3
+let i3 = 20;
+do { 
+    i3--;
+    console.log(`${i3}`);
+}
+while (i3 > 0 )
