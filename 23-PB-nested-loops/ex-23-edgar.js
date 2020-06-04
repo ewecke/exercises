7c55045474d1124d44f80101b72b527eff4c0774
eweck <edgargandolfi@gmail.com>
2020-06-04 12:52:31 +0200

    ex-23-edgar.js

---
+++ 23-PB-nested-loops/ex-23-edgar.js
@@ -0,0 +1,42 @@
+`use strict`;
+
+for (let i = 0; i <= 11; i++) { // outer loop
+    let myString = '';
+    for (let k = 10; k >= i; k--) { // nested loop // inner loop
+        myString += ' ';
+    }
+    for (let j = 0; j <= i * 2; j++) { // nested loop // inner loop
+        myString += '*';
+    }
+    for (let l = 10; l > i; l--) { // nested loop // inner loop
+        myString += ' ';
+    }
+    console.log(myString);
+}
+for (let i = 11; i >= 0; i--) { // outer loop
+    let myString = '';
+    for (let k = 10; k >= i; k--) { // nested loop // inner loop
+        myString += ' ';
+    }
+    for (let j = 0; j <= i * 2; j++) { // nested loop // inner loop
+        myString += '*';
+    }
+    for (let l = 10; l > i; l--) { // nested loop // inner loop
+        myString += ' ';
+    }
+    console.log(myString);
+}
+for (let i = 0; i <= 11; i++) { // outer loop
+    let myString = '';
+    for (let k = 10; k >= i; k--) { // nested loop // inner loop
+        myString += ' ';
+    }
+    for (let j = 0; j <= i * 2; j++) { // nested loop // inner loop
+        myString += '*';
+    }
+    for (let l = 10; l > i; l--) { // nested loop // inner loop
+        myString += ' ';
+    }
+    console.log(myString);
+}
+
